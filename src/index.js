5b4f9422b9a4fd9ab541a4f19d9af069c0b3f0cb
kevin <kevingao52997@gmail.com>
2023-02-24 17:58:50 -0800

    generated new client domain

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ async function startApolloServer(typeDefs, resolvers) {
     },
     cors: {
       credentials: true,
-      origin: ['https://odyssey-lift-off-part5-client-production-7c6e.up.railway.app/']
+      origin: ['https://odyssey-lift-off-part5-client-production-3abc.up.railway.app/']
     }
   });
 
